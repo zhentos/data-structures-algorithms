f8ad7d99d56ee1026a3e565fe2c3bedb7ea69262
Eugene D <dubovsky.e@gmail.com>
2020-05-05 16:33:55 +0300

    Data structure solution created. Linked list project added.

---
+++ Model/LinkedList.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+
+namespace LinkedList.Model
+{
+    /// <summary>
+    /// Односвязный список
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LinkedList<T> : IEnumerable
+    {
+        public LinkedList()
+        {
+            DeleteAll();
+        }
+
+       
+
+        public LinkedList(T data)
+        {
+            SetHeadAndTail(data);
+            
+        }
+        //Первый элемент списка
+        public Item<T> Head { get; private set; }
+
+        //Последний элемент списка
+        public Item<T> Tail { get; private set; }
+
+        /// <summary>
+        /// Кол-во элементов списка
+        /// </summary>
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            var item = new Item<T>(data);
+            if (Tail != null)
+            {
+                Tail.Next = item;
+                Tail = item;
+                Count++;
+            }
+            else
+            {
+                SetHeadAndTail(data);
+            }
+        }
+
+        /// <summary>
+        /// Добавить в начало списка
+        /// </summary>
+        /// <param name="data"></param>
+        public void AppendHead(T data)
+        {
+            var item = new Item<T>(data);
+            item.Next = Head;
+            Head = item;
+            Count++;
+        }
+
+        public void InsertAfter(T target, T data)
+        {
+            if (Head != null)
+            {
+                var current = Head;
+                while (current != null)
+                {
+                    if (current.Data.Equals(target))
+                    {
+                        var item = new Item<T>(data);
+                        item.Next = current.Next;
+                        current.Next = item;
+                        Count++;
+                    }
+                    else
+                    {
+                        current = current.Next;
+                    }
+                }
+            }
+        }
+        public void Clear()
+        {
+           DeleteAll(); 
+        }
+
+        public void Delete(T data)
+        {
+            if (Head != null)
+            {
+                if (Head.Data.Equals(data))
+                {
+                    Head = Head.Next;
+                    Count--;
+                    return;
+                }
+                var current = Head.Next;
+                var previous = Head;
+
+                var next = Head.Next;
+                while (current != null)
+                {
+                    if (current.Data.Equals(data))
+                    {
+                        previous.Next = current.Next;
+                        Count--;
+                    }
+
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+            else
+            {
+                SetHeadAndTail(data);
+            }
+        }
+
+        private void SetHeadAndTail(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        private void DeleteAll()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Linked List" + Count + "элементов";
+        }
+    }
+}
