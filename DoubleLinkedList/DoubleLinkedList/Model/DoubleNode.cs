884a452adfbca794ac58e3bd249f90e8ed3b9f98
Eugene D <dubovsky.e@gmail.com>
2020-05-06 18:46:28 +0300

    Double linked list implementation added.

---
+++ DoubleLinkedList/DoubleLinkedList/Model/DoubleNode.cs
@@ -0,0 +1,18 @@
+﻿namespace DoubleLinkedList.Model
+{
+    public class DoubleNode<T>
+    {
+        public DoubleNode(T data)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
+        public DoubleNode<T> Next { get; set; }
+        public DoubleNode<T> Previous { get; set; }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ DoubleLinkedList/DoubleLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoubleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dn = new Model.DoubleLinkedList<int>();
+            dn.Add(1);
+            dn.Add(2);
+            dn.AddFirst(0);
+
+            foreach (var node in dn)
+            {
+                Console.Write(node + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
