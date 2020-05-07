12ac79b8aea6c52005b0dde640e05053b3778e4d
Eugene D <dubovsky.e@gmail.com>
2020-05-07 16:53:58 +0300

    Refactoring, temporary remove LinkedList, add Queue based on linked list project

---
+++ QueueBasedOnLinkedList/QueueBasedOnLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using QueueBasedOnLinkedList.Models;
+
+namespace QueueBasedOnLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue("Kate");
+            queue.Enqueue("Sam");
+            queue.Enqueue("Alice");
+            queue.Enqueue("Tom");
+
+            foreach (string item in queue)
+                Console.WriteLine(item);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            queue.Dequeue();
+            // Console.WriteLine($"Извлеченный элемент: {firstItem}");
+            Console.WriteLine();
+
+            foreach (string item in queue)
+                Console.WriteLine(item);
+        }
+    }
+}
