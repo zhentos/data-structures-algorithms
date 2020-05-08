c910440e9aa3772922a0d719f7e4851aced4772a
Eugene D <dubovsky.e@gmail.com>
2020-05-08 23:48:08 +0300

    Linked list implementation added

---
+++ LinkedList/LinkedList/Models/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+namespace LinkedList.Models
+{
+    public class LinkedList<T> : IEnumerable
+    {
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+        public Node<T> Head { get; private set; }
+        public Node<T> Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            var nodeToAdd = new Node<T>(data);
+
+            if (Head == null)
+            {
+                Head = nodeToAdd;
+                Tail = nodeToAdd;
+            }
+            else
+            {
+                Tail.Next = nodeToAdd;
+                Tail = nodeToAdd;
+            }
+
+            Count++;
+        }
+
+        public void Delete(T data)
+        {
+            if (Head != null)
+            {
+                if (Head.Data.Equals(data))
+                {
+                    Head = Head.Next;
+                    Count--;
+                    return;
+                }
+
+                var current = Head.Next;
+                var previous = Head;
+
+                while (current != null)
+                {
+                    if (current.Data.Equals(data))
+                    {
+                        previous.Next = current.Next;
+                        Count--;
+                    }
+
+                    previous = current;
+                    current = current.Next;
+                }
+
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new Models.LinkedList<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+
+            foreach (var node in m)
+            {
+                Console.Write(node + " ");
+            }
+            Console.WriteLine();
+
+            m.Delete(3);
+
+            foreach (var node in m)
+            {
+                Console.Write(node + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
