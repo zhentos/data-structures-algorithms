f2b6dabd523d4359ddc098e712a1226f82e21b0c
Eugene D <dubovsky.e@gmail.com>
2020-05-08 23:48:08 +0300

    Linked list implementation added

---
+++ LinkedList/LinkedList/Models/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Models
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
