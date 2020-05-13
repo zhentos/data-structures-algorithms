febb3643df4d509edda7ddb773456b4c40c2046c
Eugene D <dubovsky.e@gmail.com>
2020-05-13 21:51:04 +0300

    Stack based on array implementation added.

---
+++ StackBasedOnArray/Models/FixedStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace StackBasedOnArray.Models
+{
+    public class FixedStack<T>
+    {
+        private T[] items;
+        private int defaultSize = 10;
+        private int _count;
+        public FixedStack()
+        {
+            items = new T[defaultSize];
+        }
+
+        public bool IsEmpty
+        {
+            get { return _count == 0; }
+        }
+        public FixedStack(int newSize)
+        {
+            items = new T[newSize];
+        }
+
+        public int GetStackSizeCount
+        {
+            get { return _count; }
+        }
+
+        public void Push(T data)
+        {
+            if (GetStackSizeCount == items.Length)
+            {
+                throw new InvalidOperationException("Stack Overflow");
+            }
+
+            items[_count++] = data;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            T item = items[--_count];
+            items[_count] = default(T);
+            return item;
+        }
+
+        // возвращаем элемент из верхушки стека
+        public T Peek()
+        {
+            return items[_count - 1];
+        }
+    }
+}
