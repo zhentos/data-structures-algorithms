8f69937777fd48984909fb016db39986bcdce383
Eugene D <dubovsky.e@gmail.com>
2020-05-18 22:51:15 +0300

    Tasks and Problems solution added, BinaryGap project prepared

---
+++ TasksAndProblems/TasksAndProblems/BinariGap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinariGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
