042b25831f74651b3efe1d4e033bd71b776a7601
Eugene D <dubovsky.e@gmail.com>
2020-05-19 20:30:17 +0300

    Binary Gap implementation

--- TasksAndProblems/TasksAndProblems/BinariGap/Program.cs
+++ TasksAndProblems/TasksAndProblems/BinariGap/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BinariGap
 {
+    public class Solution
+    {
+        public int solution(int N)
+        {
+            var binary = Convert.ToString(N, 2);
+            var regex = new Regex("(?<=1)(0+)(?=1)");
+
+            int result = regex.Matches(binary).Cast<Match>().Select(x => x.Length).DefaultIfEmpty(0).Max();
+            return result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p = new Solution();
+            var result = p.solution(9);
+            Console.WriteLine(result);
         }
     }
 }
