242de126c34a2dff979c3456db107afb2cbfaeaa
Eugene D <dubovsky.e@gmail.com>
2020-05-20 23:41:17 +0300

    FizzBuzz implementation

---
+++ TasksAndProblems/TasksAndProblems/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static void FizzBuzz(int n)
+        {
+            for (var i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            FizzBuzz(15);
+
+            Console.ReadKey();
+        }
+    }
+}
