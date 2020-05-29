364755de1f04e44a61a1648a6a52941670f14c94
Eugene D <dubovsky.e@gmail.com>
2020-05-29 14:36:26 +0300

    Meetup schedule problem project was added

---
+++ TasksAndProblems/TasksAndProblems/MeetupSchedule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeetupSchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
