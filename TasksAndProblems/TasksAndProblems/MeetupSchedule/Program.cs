934d0dee776e1dbc417fc982a44640db4355959e
Eugene D <dubovsky.e@gmail.com>
2020-05-30 15:20:58 +0300

    raw implementation

--- TasksAndProblems/TasksAndProblems/MeetupSchedule/Program.cs
+++ TasksAndProblems/TasksAndProblems/MeetupSchedule/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MeetupSchedule
 {
     class Program
     {
+        public static int CountMeetings(List<int> firstDay, List<int> lastDay)
+        {
+            var count = 0;
+            count = firstDay.Concat(lastDay).Distinct().Count();
+            if (count > firstDay.Count)
+            {
+                count = firstDay.Count;
+            }
+            return count;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
