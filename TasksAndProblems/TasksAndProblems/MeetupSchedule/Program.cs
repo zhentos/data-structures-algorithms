0930015811b4670402124fd27874125a84803ccc
Eugene D <dubovsky.e@gmail.com>
2020-06-01 10:36:38 +0300

    commented current implementation result

--- TasksAndProblems/TasksAndProblems/MeetupSchedule/Program.cs
+++ TasksAndProblems/TasksAndProblems/MeetupSchedule/Program.cs
@@ -6,6 +6,9 @@ namespace MeetupSchedule
 {
     class Program
     {
+        //Passed 8 from 12 tests
+        //For the input firstDay = [1,1,1] lastDay = [5,5,5]
+        //this code returns 2 however correct answer is 3
         public static int CountMeetings(List<int> firstDay, List<int> lastDay)
         {
             var count = 0;
@@ -18,6 +21,7 @@ namespace MeetupSchedule
         }
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
