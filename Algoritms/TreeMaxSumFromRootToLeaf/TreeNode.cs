92bcc69416c7d165abcedd7e9e277143c27072a5
Evgeniy D. <dubovsky.e@gmail.com>
2024-03-31 20:52:55 +0300

    Find max Treem Path from Root to Leaf

---
+++ Algoritms/TreeMaxSumFromRootToLeaf/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿
+namespace TreeMaxSumFromRootToLeaf
+{
+    public class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode? Left { get; set; }
+        public TreeNode? Right { get; set; }
+
+        public TreeNode(int data, TreeNode left, TreeNode right)
+        {
+            Left = left;
+            Right = right;
+            Val = data;
+        }
+        public TreeNode(int data)
+        {
+            Val = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
