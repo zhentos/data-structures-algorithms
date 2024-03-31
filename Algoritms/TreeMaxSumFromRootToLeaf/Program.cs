78abff0bf72a5e99d66106e1e0b3c025d9b8e633
Evgeniy D. <dubovsky.e@gmail.com>
2024-03-31 20:52:55 +0300

    Find max Treem Path from Root to Leaf

---
+++ Algoritms/TreeMaxSumFromRootToLeaf/Program.cs
@@ -0,0 +1,23 @@
+﻿using TreeMaxSumFromRootToLeaf;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var tree = Utils.Build();
+
+        var maxSum = MaxPathSum(tree);
+
+        Console.WriteLine(maxSum);
+    }
+
+    static int MaxPathSum(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        int maxPathLeft = MaxPathSum(root.Left);
+        int maxPathRight = MaxPathSum(root.Right);
+        return Math.Max(maxPathLeft, maxPathRight) + root.Val;
+    }
+}
---
+++ Algoritms/TreeMaxSumFromRootToLeaf/Utils.cs
@@ -0,0 +1,16 @@
+﻿namespace TreeMaxSumFromRootToLeaf
+{
+    public static class Utils
+    {
+        public static TreeNode Build()
+        {
+            var tree = new TreeNode(1,
+                new TreeNode(4,
+                    new TreeNode(2), new TreeNode(3,
+                                            new TreeNode(2), null)),
+                new TreeNode(7,
+                    new TreeNode(5), new TreeNode(4)));
+            return tree;
+        }
+    }
+ }
