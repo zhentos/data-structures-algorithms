deed76b2a355698cb5c93428e511bf5e6a7b8102
Evgeniy D. <eugene.dubousky@salesflow.io>
2026-06-16 12:09:07 +0300

    different problems solved.

---
+++ AddBinary_67/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var a = "11";
+        var b = "1";
+        var output = AddBinary(a, b);
+        Console.WriteLine(output);
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static string AddBinary(string a, string b)
+    {
+        var sb = new StringBuilder();
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int sum = carry;
+
+            if (i >= 0)
+            {
+                sum += a[i] - '0'; // бит из a
+                i--;
+            }
+
+            if (j >= 0)
+            {
+                sum += b[j] - '0'; // бит из b
+                j--;
+            }
+
+            int bit = sum % 2;   // текущий бит результата
+            carry = sum / 2;     // перенос в следующий разряд
+
+            sb.Append(bit);      // добавляем справа налево
+        }
+
+        // мы строили строку с конца, поэтому переворачиваем
+        var chars = sb.ToString().ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
---
+++ ValidAnagram_242/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "anagram", t = "nagaram";
+
+        Console.WriteLine(IsAnagram(s, t)); 
+    }
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        char[] orderedS = s.ToCharArray();
+        char[] orderedT = t.ToCharArray();
+
+        Array.Sort(orderedS);
+        Array.Sort(orderedT);
+
+        string sortedS = new(orderedS);
+        string sortedT = new(orderedT);
+
+        if (sortedS == sortedT)
+            return true;
+
+        return false;
+    }
+}
