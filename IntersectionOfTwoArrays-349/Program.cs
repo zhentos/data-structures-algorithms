f8055e49c3308ca9c78c378198222e2cd28e882a
Evgeniy D. <eugene.dubousky@salesflow.io>
2026-06-16 21:30:24 +0300

    2 pointers tasks solved.

---
+++ IntersectionOfTwoArrays-349/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var result = new List<int>();
+
+        var set = new HashSet<int>(nums1);
+
+        foreach (var item in nums2)
+        {
+            if (set.Contains(item))
+            {
+                result.Add(item);
+                set.Remove(item);
+            }
+        }
+
+        return [.. result];
+    }
+}
