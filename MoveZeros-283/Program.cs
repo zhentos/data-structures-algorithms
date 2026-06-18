a3276a78d8769d672a1f389174c9466af80bfb8f
Evgeniy D. <eugene.dubousky@salesflow.io>
2026-06-18 22:06:54 +0300

    1. Move zeros, TwoSum

---
+++ MoveZeros-283/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = [0, 1, 0, 3, 12];
+        MoveZeroes(nums);
+        Console.WriteLine('a');
+    }
+    public static void MoveZeroes(int[] nums)
+    {
+        var slow = 0;
+
+        for (int fast = 0; fast < nums.Length; fast++) 
+        {
+            if (nums[fast] != 0) 
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+
+        while(slow < nums.Length)
+        {
+            nums[slow] = 0;
+            slow++;
+        }
+        
+    }
+}
---
+++ TwoSum2-InputArrayIsSorted-167/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numbers = new int[] { 2, 7, 11, 15 };
+        TwoSum(numbers, 9);
+    }
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        var l = 0;
+        var r = numbers.Length - 1;
+
+        while (l < r)
+        {
+            if (numbers[l] + numbers[r] == target)
+            {
+                return [l + 1, r + 1];
+            }
+            else if (numbers[l] + numbers[r] > target)
+            {
+                r--;
+            }
+            else if (numbers[l] + numbers[r] < target)
+            {
+                l++;
+            }
+        }
+
+        return [];
+    }
+}
