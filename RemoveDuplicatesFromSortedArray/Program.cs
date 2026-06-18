cf75a73799cc3981b90c37edbb5b0e8485f9ca4f
Evgeniy D. <dubovsky.e@gmail.com>
2026-06-18 22:06:54 +0300

    1. Move zeros, TwoSum

---
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int RemoveDuplicates(int[] nums)
+    {
+        if(nums.Length == 0) return 0;
+
+        var l = 1; // начинаем запись с 1й позиции, 0я и так уникальна
+
+        for(int r = 1; r <nums.Length; r++) 
+        {
+            if (nums[r] != nums[r-1]) 
+            {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+        return l;
+    }
+}
