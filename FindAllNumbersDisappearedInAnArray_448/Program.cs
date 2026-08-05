f31de84e0c2362da8c3a57e062afe29df2e2906e
Eugene Dubousky <dubovsky.e@gmail.com>
2026-08-05 20:54:30 +0300

    Remove linked list elements; find disappeared numbers in an array.

---
+++ FindAllNumbersDisappearedInAnArray_448/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = [4, 3, 2, 7, 8, 2, 3, 1];
+        var result = FindDisappearedNumbers(nums);
+    }
+
+    public static IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        var result = new List<int>();
+        var presentedNumsSet = new HashSet<int>(nums);
+        var total = nums.Length;
+
+        for (int i = 0; i < total; i++)
+        {
+            if (!presentedNumsSet.Contains(i))
+                result.Add(i);
+        }
+        return result;
+    }
+}
---
+++ RemoveLinkedListElements-203/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace RemoveLinkedListElements_203;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ListNode head = new ListNode(1,
+         new ListNode(2,
+             new ListNode(6,
+                 new ListNode(3,
+                     new ListNode(4)))));
+
+        var result = RemoveElements(head, 6);
+
+    }
+
+    public static ListNode RemoveElements(ListNode head, int val)
+    {
+        if (head is null || head.next is null)
+            return head;
+
+        var current = head;
+
+        while (current.next != null)
+        {
+            if (current.val == val)
+            {
+                if (current.next.next == null)
+                {
+                    current.next = null;
+                }
+                else
+                {
+                    current = current.next.next;
+                }
+            }
+            current.next = current.next == null ? null : current.next.next;
+        }
+        return current;
+    }
+}
