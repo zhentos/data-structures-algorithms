cc92d8e1113d5f0181e1f9121ea83347090581d2
Eugene Dubousky <dubovsky.e@gmail.com>
2026-08-13 23:12:35 +0300

    Pascal's triangle 2

---
+++ PascalsTriangle2_119/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var rowIndex = 3;
+        var result = GetRow(rowIndex);
+    }
+
+    public static IList<int> GetRow(int rowIndex)
+    {
+        // Создаём массив размером rowIndex + 1 (все элементы = 0)
+        int[] result = new int[rowIndex + 1];
+
+        // Первый элемент всегда 1
+        result[0] = 1;
+
+        // Строим строки одну за другой до нужной
+        for (int i = 1; i <= rowIndex; i++)
+        {
+            // Идём справа налево, чтобы использовать старые значения
+            for (int j = i; j > 0; j--)
+            {
+                result[j] = result[j] + result[j - 1];
+            }
+        }
+
+        return [.. result];
+    }
+}
