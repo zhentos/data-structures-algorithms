76501b494710a377d2337ab3347b634231d0debe
Eugene Dubousky <dubovsky.e@gmail.com>
2026-08-17 22:13:44 +0300

    Nim game - 292

---
+++ NimGame_292/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var result = CanWinNim(4);
+        Console.WriteLine(result);
+    }
+
+    private static bool CanWinNim(int n)
+    {
+        return n % 4 != 0; //раз мы ходим первыми, мы можем выиграть только тогда, когда число не кратно 4
+    }
+}
