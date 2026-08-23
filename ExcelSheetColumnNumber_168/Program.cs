0229ab1a466314a4c4e6afcb42293497c5b6b08e
Eugene Dubousky <dubovsky.e@gmail.com>
2026-08-23 12:31:52 +0300

    excel sheet column title. (168)

---
+++ ExcelSheetColumnNumber_168/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var y = 25;
+        var result = ConvertToTitle(y);
+        Console.WriteLine(result);
+    }
+
+    public  static string ConvertToTitle(int columnNumber)
+    {
+        var result = new StringBuilder();
+
+        while (columnNumber > 0)
+        {
+            columnNumber--; // Shift to zero-based: A = 0, ..., Z = 25
+
+            char letter = (char)('A' + columnNumber % 26);
+            result.Append(letter);
+
+            columnNumber /= 26;
+        }
+
+        var chars = result.ToString().ToCharArray();
+        Array.Reverse(chars);
+
+        return new string(chars);
+    }
+}
